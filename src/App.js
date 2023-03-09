d22832f1f047384f970fe64b57f9075d55bd6604
Quan Nguyen <quannguyen@Quans-MacBook-Pro.local>
2023-03-09 19:39:52 +0700

    memory game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
+import { dataImg } from "./data.js";
 
 function App() {
+  const [turn, setTurn] = useState(0);
+  const [cards, setCards] = useState([]);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disable, setDisable] = useState(false);
+
+  const handleStart = () => {
+    const newDataImg = [...dataImg, ...dataImg]
+      .sort(() => {
+        return 0.5 - Math.random();
+      })
+      .map((item) => {
+        return { ...item, id: Math.random() };
+      });
+
+    setTurn(0);
+    setCards(newDataImg);
+    setChoiceOne(null);
+    setChoiceTwo(null);
+  };
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurn((prev) => prev + 1);
+    setDisable(false);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.id !== choiceTwo.id) {
+        setDisable(true);
+        if (choiceOne.value === choiceTwo.value) {
+          setCards((prev) =>
+            prev.map((card) => {
+              if (card.value === choiceOne.value) {
+                return { ...card, match: true };
+              } else {
+                return card;
+              }
+            })
+          );
+          resetTurn();
+        } else {
+          setTimeout(() => {
+            resetTurn();
+          }, 1000);
+        }
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+  
+  const completed = cards.every((item) => item.match === true); //checked users complete the game or not
+  useEffect(() => {
+    handleStart();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#333] h-[100vh]">
+      <div className="container mx-auto pt-5">
+        <h1 className="text-white text-center w-full text-3xl">Memory Game</h1>
+        <div className="w-[150px] h-10 mx-auto my-3 text-white border border-white rounded-md duration-500 hover:scale-105">
+          <button className="w-full h-full " onClick={handleStart}>
+            {!completed ? "Start" : "Restart"}
+          </button>
+        </div>
+        <p className="text-white text-center">Turn: {turn}</p>
+        {!completed ? (
+          <div className="flex">
+            <div className="grid grid-cols-4 gap-5 w-[800px] mx-auto mt-5">
+              {cards?.map((item) => {
+                return (
+                  <Card
+                    key={item.id}
+                    item={item}
+                    handleChoice={handleChoice}
+                    matched={
+                      item === choiceOne || item === choiceTwo || item.match
+                    }
+                    disable={disable}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <div className=" mt-32  text-4xl text-green-500 animate-bounce text-center">
+            Congratulation! You finished in {turn} turns
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const dataImg = [
+    {
+      src: "https://cdn.dribbble.com/users/383277/screenshots/14051354/media/4ded79488d5a6ee5a6026380a4ae4393.png?compress=1&resize=800x600&vertical=top",
+      value: 1,
+      match: false,
+    },
+    {
+      src: "https://cdn.dribbble.com/users/383277/screenshots/14051344/media/947bc7c0a2306e476b7527e5112214a3.png?compress=1&resize=640x480&vertical=top",
+      value: 2,
+      match: false,
+    },
+    {
+      src: "https://cdn.dribbble.com/users/383277/screenshots/14079108/media/c33b760e744ea761dfa5d93144370edf.png?compress=1&resize=640x480&vertical=top",
+      value: 3,
+      match: false,
+    },
+    {
+      src: "https://cdn.dribbble.com/users/383277/screenshots/14051355/media/72683f150f5460a428153ea3703f5dc6.png?compress=1&resize=640x480&vertical=top",
+      value: 4,
+      match: false,
+    },
+    {
+      src: "https://cdn.dribbble.com/users/383277/screenshots/14051336/media/5df49d065d74927a9bc28729d045c0b8.png?compress=1&resize=640x480&vertical=top",
+      value: 5,
+      match: false,
+    },
+    {
+      src: "https://cdn.dribbble.com/users/2261393/screenshots/6533934/magic_01.jpg?compress=1&resize=640x480&vertical=top",
+      value: 6,
+      match: false,
+    },
+  ];
